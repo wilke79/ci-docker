d31203376fcb71802001fb34496098fbd54f8d57
Bernd Oeggl <bernd@webman.at>
2022-02-18 08:56:36 +0100

    e2e tests with upload/exif/delete

--- test/e2e.js
+++ test/e2e.js
@@ -3,18 +3,27 @@ import { Selector } from 'testcafe';
 fixture `Webpage`
   .page('http://localhost:3001/')
 
-test(`Startseite H1`, async t => {
-  const h1 = Selector("h1");
+test(`Header 1 on main page`, async t => {
+  const h1 = Selector('h1');
   await t
     .expect(h1.exists).ok()
-    .expect(h1.textContent).eql('Bilder Datenbank');
+    .expect(h1.textContent).eql('Simple picture db');
 })
 
-test(`Datei Upload`, async t => {
-  const pics = Selector("#pics>li")
+test(`Upload picture with exif date check`, async t => {
+  const pics = Selector('#pics>li')
+  const picDate = Selector('#pics>li>span')
   await t
     .setFilesToUpload('input[type="file"]', [
       'demo.jpg'
     ])
     .expect(pics.count).eql(1)
+    .expect(picDate.textContent).eql('Sat Feb 12 2022')
+})
+
+test(`Delete picture`, async t => {
+  const pics = Selector('#pics>li')
+  await t
+    .click('#pics>li>button')
+    .expect(pics.count).eql(0)
 })
